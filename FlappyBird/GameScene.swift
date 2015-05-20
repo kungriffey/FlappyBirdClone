bfed801b55e2aabfc29096ec62ea9d41cf601b04
kunwardeep <kunwardeep.gill@gmail.com>
2015-05-20 10:39:53 -0400

    created bird animation

--- FlappyBird/GameScene.swift
+++ FlappyBird/GameScene.swift
@@ -9,14 +9,29 @@
 import SpriteKit
 
 class GameScene: SKScene {
+  
+  // Create a sprite node
+  var bird = SKSpriteNode()
+  
+  
     override func didMoveToView(view: SKView) {
-        /* Setup your scene here */
-        let myLabel = SKLabelNode(fontNamed:"Chalkduster")
-        myLabel.text = "Hello, World!";
-        myLabel.fontSize = 65;
-        myLabel.position = CGPoint(x:CGRectGetMidX(self.frame), y:CGRectGetMidY(self.frame));
-        
-        self.addChild(myLabel)
+      /* Setup your scene here */
+      var birdTexture = SKTexture(imageNamed: "flappy1")
+      // assign texture to the node
+      bird = SKSpriteNode(texture: birdTexture)
+      bird.position = CGPoint(x: CGRectGetMidX(self.frame), y: CGRectGetMidY(self.frame))
+      
+      
+      // Create second texture
+      var birdTexture2 = SKTexture(imageNamed: "flappy2")
+      // Create animation SKAction var
+      var animation = SKAction.animateWithTextures([birdTexture,birdTexture2], timePerFrame: 0.2)
+      // Make the bird flap SKAction var
+      var makeBirdFlap = SKAction.repeatActionForever(animation)
+      bird.runAction(makeBirdFlap)
+      
+      addChild(bird)
+
     }
     
     override func touchesBegan(touches: Set<NSObject>, withEvent event: UIEvent) {
@@ -25,17 +40,6 @@ class GameScene: SKScene {
         for touch in (touches as! Set<UITouch>) {
             let location = touch.locationInNode(self)
             
-            let sprite = SKSpriteNode(imageNamed:"Spaceship")
-            
-            sprite.xScale = 0.5
-            sprite.yScale = 0.5
-            sprite.position = location
-            
-            let action = SKAction.rotateByAngle(CGFloat(M_PI), duration:1)
-            
-            sprite.runAction(SKAction.repeatActionForever(action))
-            
-            self.addChild(sprite)
         }
     }
    
